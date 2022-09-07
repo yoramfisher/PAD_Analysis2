152b83d7c61df514daa7bcf23542536f70c815b7
Iain Marcuson <iain.marcuson@sydortechnologies.com>
2022-09-07 12:10:20 -0400

    Add single pixel detection.

---
+++ Mask_Anal.py
@@ -0,0 +1,98 @@
+import numpy as np
+import Big_keck_load as BKL
+import os
+
+class EventPixel:
+    def __init__(self):
+        self.x = -1;
+        self.y = -1;
+        self.cap = -1;
+        self.value = -1;        # Initialize to invalid values
+
+# Set thresholds
+low_thresh = [400, 400, 400, 400, 400, 400, 400, 400]; # Threshold for neighbors being low
+high_thresh = [600, 600, 600, 600, 600, 600, 600, 600]; # Threshold for pixel under test being high
+
+# Allocate arrays
+backStack = np.zeros((8,512,512), dtype=np.double);
+foreStack = np.zeros((8,512,512), dtype=np.double);
+
+# Load background
+backImageFilename = 'keck_single_pixel/run-scan_issbufPIX_b_800/frames/scan_issbufPIX_b_800_00000001.raw';
+numBackImages = int(os.path.getsize(backImageFilename)/(1024+512*512*2));
+backImageFile = open(backImageFilename, "rb");
+
+for fIdx in range(numBackImages):
+    payload = BKL.keckFrame(backImageFile);
+    backStack[(payload[3]-1)%8,:,:] += np.resize(payload[4],[512,512]);
+backStack = backStack/(numBackImages/8); # Average the background
+backImageFile.close();
+
+# Load the foreground images
+foreImageFilename = 'keck_single_pixel/run-scan_issbufPIX_f_800/frames/scan_issbufPIX_f_800_00000001.raw';
+numForeImages = int(os.path.getsize(foreImageFilename)/(1024+512*512*2));
+foreImageFile = open(foreImageFilename, "rb");
+
+for fIdx in range(numBackImages):
+    payload = BKL.keckFrame(foreImageFile);
+    foreStack[(payload[3]-1)%8,:,:] += np.resize(payload[4],[512,512]);
+foreStack = foreStack/(numForeImages/8); # Average the background
+foreImageFile.close();
+
+# Compute the background subtracted image
+fmbImage = foreStack - backStack;
+
+# Save the computed images
+foreStack.tofile('fore_avg.raw');
+backStack.tofile('back_avg.raw');
+fmbImage.tofile('fmb.raw');
+
+singlePixelList = [];
+noPixelList = [];
+total_pixels = 0;
+cold_neighborhoods = 0;
+    
+#Now iterate over the inner pixels to find single events
+for cap_idx in range(8):
+    curr_frame = fmbImage[cap_idx,:,:]; # Get just the current frame
+    for row_idx in range(2,510):    # Ignore outer two pixels
+        for col_idx in range(2,510): # Ibid
+            total_pixels += 1;       # Increment pixel count
+            test_mat = curr_frame[(row_idx-1):(row_idx+1+1),(col_idx-1):(col_idx+1+1)]; # Extract the 3x3 neighborhood
+            # Compute the low threshold
+            cold_test = (test_mat<low_thresh[cap_idx]).astype(int); # See if neighborhood below limi
+            cold_test[1,1] = 0;              # Set inner to zero for sum
+            cold_sum = np.sum(cold_test);
+            #if (cold_sum > 0):
+            #    print(cold_test);
+            cold_neighbor = False;
+            if cold_sum == 8:   # All pixels below threshold
+                cold_neighbor = True;
+                cold_neighborhoods += 1;
+                test_pixel = EventPixel();
+                test_pixel.x = col_idx;
+                test_pixel.y = row_idx;
+                test_pixel.cap = cap_idx;
+                test_pixel.value = test_mat[1,1]; # Set the pixel parameters
+                
+                # Determine list to append to
+                if test_pixel.value >= high_thresh[cap_idx]: # Pixel hot
+                    singlePixelList.append(test_pixel);
+                else:           # Pixel cold
+                    noPixelList.append(test_pixel);
+
+print("Tested {} pixels.".format(total_pixels));
+print("Cold neighborhoods: {}".format(cold_neighborhoods));
+print("{} single pixels and {} no pixels.".format(len(singlePixelList),len(noPixelList)));
+single_pixel_filename = 'single_pix.csv';
+single_pixel_file = open(single_pixel_filename, 'w');
+for curr_pixel in singlePixelList:
+    single_pixel_file.write('{},{},{},{}\n'.format(curr_pixel.y, curr_pixel.x, curr_pixel.cap, curr_pixel.value));
+single_pixel_file.close();
+
+no_pixel_filename = 'no_pix.csv';
+no_pixel_file = open(no_pixel_filename, 'w');
+for curr_pixel in noPixelList:
+    no_pixel_file.write('{},{},{},{}\n'.format(curr_pixel.y, curr_pixel.x, curr_pixel.cap, curr_pixel.value));
+no_pixel_file.close();
+
