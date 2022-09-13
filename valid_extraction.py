5c57b8388e0f5a4fb8c54fb2f18c83922e64c532
Iain Marcuson <iain.marcuson@sydortechnologies.com>
2022-09-13 10:11:09 -0400

    First cut of plot migration to loops.

--- valid_extraction.py
+++ valid_extraction.py
@@ -12,6 +12,8 @@ def clip_hist(hist_data, clip_thresh):
     return clipped_pixels;
 
 
+# Specify the number of caps
+NUM_CAPS = 8
 
 # Initialize the filenames
 #bgFilename = '/mnt/raid/keckpad/set-phHist/run-4ms_back/frames/4ms_back_00000001' +'.raw'
@@ -23,13 +25,13 @@ pFolder = "vref_50kv"
 dFolder = "vref"
 backImageData = open(bgFilename,"rb")
 
-backStack = np.zeros((8,512,512),dtype=np.double)
+backStack = np.zeros((NUM_CAPS,512,512),dtype=np.double)
 numImages = int(os.path.getsize(bgFilename)/(1024+512*512*2))
 
 #Calc cap backs
 for fIdex in range(numImages):
    payload = BKL.keckFrame(backImageData)
-   backStack[(payload[3]-1)%8,:,:] += np.resize(payload[4],[512,512])
+   backStack[(payload[3]-1)%NUM_CAPS,:,:] += np.resize(payload[4],[512,512])
 backStack = backStack/ (numImages/8)
 
 
@@ -56,90 +58,40 @@ for num in range(numFiles):
     for fIdx in range(numFgImages):
         payload = BKL.keckFrame(fgImageFile);
         curr_frame = payload[4].reshape([512,512]);
-        fmbImg = curr_frame - backStack[(payload[3]-1)%8,:,:];
+        fmbImg = curr_frame - backStack[(payload[3]-1)%NUM_CAPS,:,:];
         #fmbImg = fmbImg[128:(128+128+1),256:(256+128+1)] #uncomment for looking at individual pixels
         #-=-= XXX Comment this out for production
         #fmbImg = CreateSim.CreateSim();
 
-        pixelExtractor.extract_frame(fmbImg, (payload[3]-1)%8); # FIXME Assumes that all caps are being used in the foreground image
+        pixelExtractor.extract_frame(fmbImg, (payload[3]-1)%NUM_CAPS); # FIXME Assumes that all caps are being used in the foreground image
         
     # Close the file
     fgImageFile.close();
 
 # Now get some valid pixels
-valid_pixels0 = np.array(pixelExtractor.valid_values[0])
-valid_pixels1 = np.array(pixelExtractor.valid_values[1])
-valid_pixels2 = np.array(pixelExtractor.valid_values[2])
-valid_pixels3 = np.array(pixelExtractor.valid_values[3])
-valid_pixels4 = np.array(pixelExtractor.valid_values[4])
-valid_pixels5 = np.array(pixelExtractor.valid_values[5])
-valid_pixels6 = np.array(pixelExtractor.valid_values[6])
-valid_pixels7 = np.array(pixelExtractor.valid_values[7])
-# allpixels = []
-# for val in range(8):
-#     print('{} {}'.format(val, len(pixelExtractor.valid_values[val])))
-#     print(pixelExtractor.valid_values[val][0:10])
-#     allpixels.extend(pixelExtractor.valid_values[val])
+valid_pixels = [];
+for cap_idx in range(NUM_CAPS):
+    valid_pixels.append(np.array(pixelExtractor.valid_values[cap_idx]))
 
 # valid_pixelsall= np.array(allpixels).reshape([1,-1])
 clipPos = 250
 clipNeg = -50
+clip_thresh = 0.000;
+
 # Clip the arrays
-clipped0 = clip_hist(valid_pixels0, 0.00);
-clipped1 = clip_hist(valid_pixels1, 0.00);
-clipped2 = clip_hist(valid_pixels2, 0.00);
-clipped3 = clip_hist(valid_pixels3, 0.00);
-clipped4 = clip_hist(valid_pixels4, 0.00);
-clipped5 = clip_hist(valid_pixels5, 0.00);
-clipped6 = clip_hist(valid_pixels6, 0.00);
-clipped7 = clip_hist(valid_pixels7, 0.00);
-# clipped0 = clipped0[clipped0>clipNeg]
-# clipped0 = clipped0[clipped0<clipPos]
-# clipped1 = clipped1[clipped1<clipPos]
-# clipped1 = clipped1[clipped1>clipNeg]
-# clipped2 = clipped2[clipped2>clipNeg]
-# clipped2 = clipped2[clipped2<clipPos]
-# clipped3 = clipped3[clipped3>clipNeg]
-# clipped3 = clipped3[clipped3<clipPos]
-# clipped4 = clipped4[clipped4>clipNeg]
-# clipped5 = clipped5[clipped5>clipNeg]
-# clipped6 = clipped6[clipped6>clipNeg]
-# clipped7 = clipped7[clipped7>clipNeg]
-# clipped4 = clipped4[clipped4<clipPos]
-# clipped5 = clipped5[clipped5<clipPos]
-# clipped6 = clipped6[clipped6<clipPos]
-# clipped7 = clipped7[clipped7<clipPos]
+clipped_pixels = [];
+for cap_idx in range(NUM_CAPS):
+    clipped_pixels.append(clip_hist(valid_pixels[cap_idx], clip_thresh));
 
-
-fig,axs = plt.subplots(8,1)
-
-
-# axs[0].set_yscale('log')
-# axs[1].set_yscale('log')
-# axs[2].set_yscale('log')
-# axs[3].set_yscale('log')
-# axs[4].set_yscale('log')
-# axs[5].set_yscale('log')
-# axs[6].set_yscale('log')
-# axs[7].set_yscale('log')
-
+# Do the plotting
+fig,axs = plt.subplots(NUM_CAPS,1)
+# Special case if only one cap
+if NUM_CAPS == 1:
+    axs = [axs]                 # Turn into a list so it can be subscripted
 binRan = np.arange(-50,351)
 
-axs[0].hist(clipped0, bins=binRan);
-axs[1].hist(clipped1, bins=binRan);
-axs[2].hist(clipped2, bins=binRan);
-axs[3].hist(clipped3, bins=binRan);
-axs[4].hist(clipped4, bins=binRan);
-axs[5].hist(clipped5, bins=binRan);
-axs[6].hist(clipped6, bins=binRan);
-axs[7].hist(clipped7, bins=binRan);
+for cap_idx in range(NUM_CAPS):
+    axs[cap_idx].hist(clipped_pixels[cap_idx], bins=binRan);
 
-
-#nphist, bins = np.histogram(clipped7, bins=400, range=(-6,71))
-#axs[7].plot(bins[:-1], nphist);
-# axs[2].hist(valid_pixelsall, bins=400);
-
-#fig,axs = plt.subplots(1,1)
-#axs.hist(clipped3, bins=binRan);
 
 plt.show()
