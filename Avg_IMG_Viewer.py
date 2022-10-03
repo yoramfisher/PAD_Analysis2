1547bf9f545b915309cf86496fcf19e8b8ed5d33
Sydor mmpad_dev001 <dev001.sydor@sydortechnologies.com>
2022-10-03 15:52:08 -0400

    now showing more images

--- Avg_IMG_Viewer.py
+++ Avg_IMG_Viewer.py
@@ -7,8 +7,8 @@ import os
 import matplotlib.pyplot as plt
 import sys
 
-foreFile = "/mnt/raid/keckpad/set-geocal_dcsKeck/run-geocal_f/frames/geocal_f_00000001.raw"
-backFile = "/mnt/raid/keckpad/set-geocal_dcsKeck/run-geocal_b/frames/geocal_b_00000001.raw"
+foreFile = "/mnt/raid/keckpad/set-junk123/run-1ms_flat_fore/frames/1ms_flat_fore_00000001.raw"
+backFile = "/mnt/raid/keckpad/set-junk123/run-1ms_flat_back/frames/1ms_flat_back_00000001.raw"
 cwd = os.getcwd()
 foreImage = open(foreFile,"rb")
 backImage = open(backFile,"rb")
@@ -30,31 +30,47 @@ for fIdex in range(numImagesF):
 
 avgFore = foreStack/(numImagesB/8.0)
 plotData = avgFore-avgBack
-plotData1 = plotData[0,:,:]
-plotData = np.average(plotData, axis=0)
-clipData = np.clip(plotData, 0, 1250)
-# plt.imshow(plotData)
-# # fig,axs = plt.subplots(2,4)
-# # axs[0,0].imshow(plotData[0,:,:])
-# # axs[0,1].imshow(plotData[1,:,:])
-# # axs[0,2].imshow(plotData[2,:,:])
-# # axs[0,3].imshow(plotData[3,:,:])
-# # axs[1,0].imshow(plotData[4,:,:])
-# # axs[1,1].imshow(plotData[5,:,:])
-# # axs[1,2].imshow(plotData[6,:,:])
-# # axs[1,3].imshow(plotData[7,:,:])
-# #plt.imshow(fmb[3,:,:])
-# plt.show()
+plotDataClip = np.clip(plotData, 0, 1000)
+avgplotData = np.average(plotDataClip, axis=0)
+avg,axs = plt.subplots(1)
+imageAvg = axs.imshow(avgplotData, cmap = "viridis")
+Acbar = avg.colorbar(imageAvg, aspect=10)
+axs.set_title('DCS Keck Cap Average')
+axs.set_ylabel("Pixel")
+axs.set_xlabel("Pixel")
+Acbar.set_label ("Counts (ADU)")
+# fig.set_size_inches(20, 10)    
+# fig.subplots_adjust(wspace = 0.545)
+avg.savefig(foreFile + "_Avg.png")
 
-fig,ax = plt.subplots(1)
+# plt.imshow(plotData)
+
+
+allplot = []
+for val in range(8):
+   allplot.append(plotDataClip[val,:,:])
+
+
+indexVal =  (-1)
+fig,ax = plt.subplots(2,4)
+for pic in allplot:
+   indexVal += 1 
+   indexRow = int(indexVal/4) 
+   indexCol = int(indexVal%4)
+   #indexVal = allplot.index(pic)
+   image = ax[indexRow,indexCol].imshow(pic, cmap = "viridis")
+   # ax.imshow(pic)
+# fig,ax = plt.subplots(1)
 #needed to add more stuff
-image = ax.imshow(clipData, cmap = "viridis")
-cbar = fig.colorbar(image, aspect=10)
-ax.set_title('DCS Keck')
-ax.set_ylabel("Pixel")
-ax.set_xlabel("Pixel")
-cbar.set_label ("Counts (ADU)")
-fig.savefig(foreFile + "_Avg.png")
+# image = ax.imshow(clipData, cmap = "viridis")
+   cbar = fig.colorbar(image, aspect=10, ax = ax[indexRow,indexCol])
+   ax[indexRow,indexCol].set_title('DCS Keck Cap'+ str(indexVal))
+   ax[indexRow,indexCol].set_ylabel("Pixel")
+   ax[indexRow,indexCol].set_xlabel("Pixel")
+   cbar.set_label ("Counts (ADU)")
+fig.set_size_inches(20, 10)    
+fig.subplots_adjust(wspace = 0.545)
+fig.savefig(foreFile + "_AvgAll.png")
 plt.show()
 
 #################
